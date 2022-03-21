a19f552fc2570084787750cd58f0bc12f188b647
aotero <aotero@sooft.com.ar>
2022-03-21 20:50:02 -0300

    "Toma y muestra de datos en archivos externos"

--- ej1/main.c
+++ ej1/main.c
@@ -43,11 +43,34 @@ char *parse_filepath(int argc, char *argv[]) {
 
 unsigned int array_from_file(int array[],
            unsigned int max_size, const char *filepath) {
+    FILE *fileOpen = NULL;
+    unsigned int length;
 
+    fileOpen = fopen(filepath, "r");
+
+    if (fileOpen == NULL) {
+    printf("El archivo no existe");
+    exit(EXIT_FAILURE);
+    }
+
+    fscanf(fileOpen, "%u", &length);
+    //Utilizo este if para no dejar en desuso max_size(lo toma los warnings del compilador)
+    if(length > max_size) {
+      printf("El largo del archivo es mas grande del permitido");
+      exit(EXIT_FAILURE);
+    }
+
+    for (unsigned int i = 0; i < length; i++) {
+      fscanf(fileOpen, "%d", &array[i]);
+    }
+
+    return length;
 }
 
 void array_dump(int a[], unsigned int length) {
-    //your code here!!!!!
+    for (unsigned int i = 0; i < length; i++) {
+      printf("%d  ", a[i]);
+    }
 }
 
 
@@ -56,7 +79,6 @@ int main(int argc, char *argv[]) {
 
     /* parse the filepath given in command line arguments */
     filepath = parse_filepath(argc, argv);
-
     /* create an array of MAX_SIZE elements */
     int array[MAX_SIZE];
 
