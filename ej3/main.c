26893d4b9eb3c079dc61245332138c18e6d2e58e
aotero <aotero@sooft.com.ar>
2022-03-26 10:47:26 -0300

    Cerrando apertura de file ej1 y ej3 completo

--- ej3/main.c
+++ ej3/main.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "array_helpers.h"
 
 /* Maximum allowed length of the array */
 #define MAX_SIZE 100000
@@ -40,39 +41,6 @@ char *parse_filepath(int argc, char *argv[]) {
 
     return result;
 }
-
-unsigned int array_from_file(int array[],
-           unsigned int max_size, const char *filepath) {
-    FILE *fileOpen = NULL;
-    unsigned int length;
-
-    fileOpen = fopen(filepath, "r");
-
-    if (fileOpen == NULL) {
-    printf("El archivo no existe");
-    exit(EXIT_FAILURE);
-    }
-
-    fscanf(fileOpen, "%u", &length);
-    //Utilizo este if para no dejar en desuso max_size(lo toma los warnings del compilador)
-    if(length > max_size) {
-      printf("El largo del archivo es mas grande del permitido");
-      exit(EXIT_FAILURE);
-    }
-
-    for (unsigned int i = 0; i < length; i++) {
-      fscanf(fileOpen, "%d", &array[i]);
-    }
-    fclose(fileOpen);
-    return length;
-}
-
-void array_dump(int a[], unsigned int length) {
-    for (unsigned int i = 0; i < length; i++) {
-      printf("%d  ", a[i]);
-    }
-}
-
 
 int main(int argc, char *argv[]) {
     char *filepath = NULL;
